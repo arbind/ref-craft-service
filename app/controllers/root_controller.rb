c091c83b5b1edae1a69cdcbb27780b04ff019b47
Arbind <arbind@carbonfive.com>
2013-04-21 19:40:52 -0700

    search endpoint

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -12,6 +12,7 @@ class RootController < ApplicationController
   end
 
   def search
+    startTime = Time.new
     @query = params[:query]
     @lat = params[:lat]
     @lng = params[:lng]
@@ -21,28 +22,48 @@ class RootController < ApplicationController
     @limit = params[:limit] || 20
     @page = @page.to_i
     @limit = @limit.to_i
+    @limit = 100 if @limit > 100
 
-    @geo_coordinates = [@lng, @lat] if @lng.present? and @lat.present?
+    @geo_coordinates = [@lat, @lng] if @lat.present? and @lng.present?
 
-    @crafts = Craft.near(@geo_coordinates, @radius).desc(:rank) if @geo_coordinates
-    # @crafts ||= Craft.near(@geo_place, @radius).desc(:ranking_score) if @geo_place
+    # search near geo
+    @crafts = Craft.near(@location, @radius).desc(:ranking_score) if @location.present?    
+    @crafts = Craft.near(@geo_coordinates, @radius).desc(:rank) if @geo_coordinates.present?
+
+    # search for given query
+    @crafts = @crafts.where(search_tags: @query) if  @query.present? and @crafts.present?
+    @crafts = Crafts.where(search_tags: @query) if @query.present? and not @crafts.present?
 
     if @crafts.present?
       @total_crafts_count = @crafts.count
       skip = (@page-1) * @limit
-      @crafts = @crafts.skip(skip).limit(limit)
+      @crafts = @crafts.skip(skip).limit(@limit)
 
       @total_pages = 1 + (@total_crafts_count/@limit).to_i
-      # js_var(total_crafts_count: @total_crafts_count, page: @page, total_pages: @total_pages)
     end
 
-    if @query.present?
-      @crafts = @crafts.where(search_tags: @query) if @crafts.present?
-      @crafts ||= Craft.where(search_tags: @query)
-    end
+    @craft_list = @crafts.to_a if @crafts.present?
+    @results = {
+      result_stats: {
+        total: @total_crafts_count,
+        count: @craft_list.count
+      },
+      query_info: {
+        page: @page,
+        limit: @limit,
+        query: @query,
+        lat: @lat,
+        lng: @lng,
+        location: @location
+      },
+      crafts: @craft_list
+    }
+    endTime = Time.new
+    duration = endTime - startTime
+    @results[:result_stats][:duration] = duration
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: nil }
+      format.json { render json: @results }
     end
   end
 
